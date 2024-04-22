a9f6b8649350ef24bf7e67997b4ebb441a92d7f7
felipesantos22 <felipevs2018@gmail.com>
2024-04-22 16:47:06 -0300

    changed header

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -2,38 +2,38 @@ import { Country } from "../interfaces/Country";
 
 export const listOfCountries: Country[] = [
     {
-        name: "Egito",
-        imagem: "https://images.pexels.com/photos/18991528/pexels-photo-18991528/free-photo-of-construcao-predio-edificio-viagem.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        name: "Brasil",
+        imagem: "https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_1280.jpg",
         data:"Jan 1, 2024",
         price: 1000
     },
     {
         name: "França",
-        imagem: "https://images.pexels.com/photos/3185480/pexels-photo-3185480.jpeg?auto=compress&cs=tinysrgb&w=600",
+        imagem: "https://cdn.pixabay.com/photo/2020/07/12/16/40/paris-5397889_1280.jpg",
         data: "Jan 1, 2024",
         price: 200
     },
     {
         name: "Marrocos",
-        imagem: "https://images.pexels.com/photos/7566890/pexels-photo-7566890.jpeg?auto=compress&cs=tinysrgb&w=600",
+        imagem: "https://cdn.pixabay.com/photo/2017/05/27/22/33/morocco-2349647_1280.jpg",
         data: "Jan 1, 2024",
         price: 500
     },
     {
         name: "Itália",
-        imagem: "https://images.pexels.com/photos/2034684/pexels-photo-2034684.jpeg?auto=compress&cs=tinysrgb&w=600",
+        imagem: "https://cdn.pixabay.com/photo/2023/10/27/15/51/italy-8345688_1280.jpg",
         data: "Jan 1, 2024",
         price: 900
     },
     {
         name: "EUA",
-        imagem: "https://images.pexels.com/photos/1755390/pexels-photo-1755390.jpeg?auto=compress&cs=tinysrgb&w=600",
+        imagem: "https://cdn.pixabay.com/photo/2014/11/21/17/23/new-york-540807_1280.jpg",
         data: "Jan 1, 2024",
         price: 1500
     },
     {
         name: "México",
-        imagem: "https://images.pexels.com/photos/19873071/pexels-photo-19873071/free-photo-of-camelo.jpeg?auto=compress&cs=tinysrgb&w=600",
+        imagem: "https://cdn.pixabay.com/photo/2015/11/05/23/02/chichen-itza-1025099_1280.jpg",
         data: "Jan 1, 2024",
         price: 2800
     },
